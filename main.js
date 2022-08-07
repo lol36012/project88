76c277151421eb4a52c0108361f0bd6aab91ef5a
lol36012 <104506829+lol36012@users.noreply.github.com>
2022-08-07 12:13:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,93 +1,76 @@
-var mouseEvent = "Empty";
-
-var last_position_of_x;
-var last_position_of_y;
+var mouseEvent = "empty";
 
     canvas = document.getElementById('myCanvas');
     ctx = canvas.getContext("2d");
     
-    var color = "black";
-    var width_of_line = 1;
+    color = "black";
+    width_of_line = 2;
 
+    /*Uncomment the correct line*/
     canvas.addEventListener("mousedown", my_mousedown);
+    // canvas.setEventListener("mousedown", my_mousedown);
+    //canvas.getEventListener("mousedown", my_mousedown);
 
     function my_mousedown(e)
     {
         color = document.getElementById("color").value;
         width_of_line = document.getElementById("width_of_line").value;
-        
-
-        mouseEvent = "mousedown";
+        radius = document.getElementById("radius").value;
+        mouseEvent = "mouseDown";
     }
 
-canvas.addEventListener("mouseup", my_mouseup);
-function my_mouseup(e)
-{
+    /*
+    Create an event listener for "mousemove"
+    and call function my_mousemove
+    */canvas.addEventListener("mousemove",my_mousemove);
 
-    mouseEvent = "mouseUP"
-}
- 
-canvas.addEventListener("mouseleave",my_mouseleave);
-function my_mouseleave(e)
-{
-    current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
-    current_position_of_mouse_y = e.clientY - canvas.offsetTop;
-
-    if (mouseEvent == "mouseDown") {
-        ctx.beginPath{};
-        ctx.stokeStyle = color;
-        ctx.lineWidth = width_of_line;
-
-        console.log("Last position of x and y coordinates = ");
-        console.log("x = "+ last_position_of_x + )
-    }
-}
-{
-    mouseEvent = "mouseleave"
-}
-
-
-
-
-    
     function my_mousemove(e)
     {
+        /*Uncomment the correct line*/
         current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
+        //current_position_of_mouse_x = e.clientX - canvas.offsetRight;
+        //current_position_of_mouse_x = e.clientX - canvas.offsetBottom;
+
+        
+         
         current_position_of_mouse_y = e.clientY - canvas.offsetTop;
 
-        //Check whether the mouseEvent is "mousedown".  
-        
-        //If yes call "beginPath()".
-        
-        // Assign values to strokeStyle and lineWidth of canvas "ctx".
-        
-
-        //print the current x and y coordinate.
-        console.log("Last position of x and y coordinates = ");
-        console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
-
-        //Call moveTo() with parameters last_position_of_x  and last_position_of_y.
-        // The drawing begins from these points.
-        
-
-        //print the current x and y coordinate.
+        if (mouseEvent == "mouseDown") {
         console.log("Current position of x and y coordinates = ");
         console.log("x  = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
-        //Call "lineTo()" function with 'current_position_of_mouse_x' and 'current_position_of_mouse_y'.
-        //Creation if lines end at these points
-        
-
-        //Now we will draw the line using the stroke() function.
-        
-        
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width_of_line;
+        ctx.arc(current_position_of_mouse_x, current_position_of_mouse_y, radius ,0 , 2 * Math.PI);
+        ctx.stroke();
         }
-        // "last_position_of_x" and "last_position_of_y" is updated with current mouse position of x and y.
-        last_position_of_x = current_position_of_mouse_x; 
-        last_position_of_y = current_position_of_mouse_y;
+
     }
 
+    function my_mouseup(e)
+    {
+        
+    }
+    /*Create an event listener for "mouseup"
+    and call function my_mouseup
+    
+    Create a function named my_mouseup with
+    event e as parameter.
+    
+    Assign "mouseUP" to mouseEvent
+    within the function
+    */
+    
+    /*Create an event listener for "mouseleave"
+    and call function my_mouseleave
 
-//Additional Activity
+    Create a function named my_mouseleave with
+    event e as parameter.
+    
+    Assign "mouseleave" to mouseEvent
+    within the function
+    */
+
 function clearArea() {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
